42d4814d3ae248250027718623633498f242413d
coolzoom <tommingc@gmail.com>
2020-01-27 02:21:17 +0800

    adding support for uwow

--- sql/base/auth.sql
+++ sql/base/auth.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : World of Warcraft Mist of Pandaria
-Source Server Version : 50532
-Source Host           : 127.0.0.1:3306
+Source Server         : 3307rootascent
+Source Server Version : 50509
+Source Host           : localhost:3307
 Source Database       : auth
 
 Target Server Type    : MYSQL
-Target Server Version : 50532
+Target Server Version : 50509
 File Encoding         : 65001
 
-Date: 2017-11-21 23:35:30
+Date: 2020-01-27 02:05:04
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -43,10 +43,17 @@ CREATE TABLE `account` (
   `mutereason` varchar(3) NOT NULL DEFAULT '',
   `muteby` varchar(3) NOT NULL DEFAULT '',
   `recruiter` int(10) unsigned NOT NULL DEFAULT '0',
-  `loyalty_points` int(10) unsigned NOT NULL DEFAULT '0',
+  `last_build` int(10) unsigned NOT NULL DEFAULT '0',
+  `activate` tinyint(3) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COMMENT='Account System';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='Account System';
+
+-- ----------------------------
+-- Records of account
+-- ----------------------------
+INSERT INTO `account` VALUES ('1', 'EMO', 'F485C6118476B0811162CBB503E99979282C736C', '274DE110114E67B783E050DBFDEB57EDB18D44829AF11296FE011C00070CEE87B3E7E04269BFE31B', '89200B923311EB8CE183786CBC8C86B9E414CB1483A84417287092B8B92A2354', '8D5D947B02038202A3B63C1A7E4B7B6E1A446CD14886361233FD2D6EC9FD2A75', '', '', '', '2017-08-19 20:14:23', '192.168.1.1', '0', '0', '0', '2017-08-19 23:10:00', '0', '4', '0', '0', 'Win', '', '', '0', '0', '1');
+INSERT INTO `account` VALUES ('2', 'ADMIN', '8301316D0D8448A34FA6D0C6BF1CBFA2B4A1A93A', '5C21E9FCD2F7EBA0DC16ADB283A1E889F605900A28FBC58183881E9C79B2D826E45E9ADDBA4BC2AA', '73F3B13FE526C493C3F911C7B0165CE2197D3F05073CD8893FD97A23349E0279', 'BA6BB3732F8C3647C2E1AB201BFDF74113EB8C649CBAC50B2B6FCC352C5BBB9B', '', '', '', '2018-11-07 00:21:47', '192.168.0.7', '0', '0', '0', '2020-01-27 01:53:22', '1', '4', '0', '4', 'Win', '', '', '0', '0', '1');
 
 -- ----------------------------
 -- Table structure for account_access
@@ -59,6 +66,13 @@ CREATE TABLE `account_access` (
   `comment` text,
   PRIMARY KEY (`id`,`RealmID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of account_access
+-- ----------------------------
+INSERT INTO `account_access` VALUES ('0', '3', '-1', null);
+INSERT INTO `account_access` VALUES ('1', '255', '-1', null);
+INSERT INTO `account_access` VALUES ('2', '6', '-1', null);
 
 -- ----------------------------
 -- Table structure for account_banned
@@ -75,6 +89,10 @@ CREATE TABLE `account_banned` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Ban List';
 
 -- ----------------------------
+-- Records of account_banned
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for account_log_ip
 -- ----------------------------
 DROP TABLE IF EXISTS `account_log_ip`;
@@ -84,6 +102,13 @@ CREATE TABLE `account_log_ip` (
   `date` datetime DEFAULT NULL,
   PRIMARY KEY (`accountid`,`ip`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of account_log_ip
+-- ----------------------------
+INSERT INTO `account_log_ip` VALUES ('1', '192.168.1.1', '2017-08-19 15:16:31');
+INSERT INTO `account_log_ip` VALUES ('2', '127.0.0.1', '2018-11-07 00:22:30');
+INSERT INTO `account_log_ip` VALUES ('2', '192.168.0.7', '2020-01-24 00:24:22');
 
 -- ----------------------------
 -- Table structure for account_premium
@@ -100,6 +125,10 @@ CREATE TABLE `account_premium` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Records of account_premium
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for account_rep_mods
 -- ----------------------------
 DROP TABLE IF EXISTS `account_rep_mods`;
@@ -109,6 +138,10 @@ CREATE TABLE `account_rep_mods` (
   `modifier` int(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`,`reputationId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of account_rep_mods
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for account_spell
@@ -125,6 +158,12 @@ CREATE TABLE `account_spell` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
+-- Records of account_spell
+-- ----------------------------
+INSERT INTO `account_spell` VALUES ('1', '42777', '1', '0');
+INSERT INTO `account_spell` VALUES ('1', '72286', '1', '0');
+
+-- ----------------------------
 -- Table structure for firewall_farms
 -- ----------------------------
 DROP TABLE IF EXISTS `firewall_farms`;
@@ -132,6 +171,10 @@ CREATE TABLE `firewall_farms` (
   `ip` tinytext NOT NULL,
   `type` tinyint(1) unsigned NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of firewall_farms
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for ip_banned
@@ -147,6 +190,10 @@ CREATE TABLE `ip_banned` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Banned IPs';
 
 -- ----------------------------
+-- Records of ip_banned
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for ip_to_country
 -- ----------------------------
 DROP TABLE IF EXISTS `ip_to_country`;
@@ -158,6 +205,10 @@ CREATE TABLE `ip_to_country` (
   `COUNTRY_NAME` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`IP_FROM`,`IP_TO`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of ip_to_country
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for logs
@@ -172,6 +223,10 @@ CREATE TABLE `logs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Records of logs
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for log_vote
 -- ----------------------------
 DROP TABLE IF EXISTS `log_vote`;
@@ -183,6 +238,10 @@ CREATE TABLE `log_vote` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
+-- Records of log_vote
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for mails
 -- ----------------------------
 DROP TABLE IF EXISTS `mails`;
@@ -191,16 +250,8 @@ CREATE TABLE `mails` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for rbac_account_groups
+-- Records of mails
 -- ----------------------------
-DROP TABLE IF EXISTS `rbac_account_groups`;
-CREATE TABLE `rbac_account_groups` (
-  `accountId` int(10) unsigned NOT NULL COMMENT 'Account id',
-  `groupId` int(10) unsigned NOT NULL COMMENT 'Group id',
-  `realmId` int(11) NOT NULL DEFAULT '-1' COMMENT 'Realm Id, -1 means all',
-  PRIMARY KEY (`accountId`,`groupId`,`realmId`),
-  KEY `fk__rbac_account_groups__rbac_groups` (`groupId`) USING BTREE
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Account-Group relation';
 
 -- ----------------------------
 -- Table structure for realmcharacters
@@ -213,6 +264,12 @@ CREATE TABLE `realmcharacters` (
   PRIMARY KEY (`realmid`,`acctid`),
   KEY `acctid` (`acctid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Realm Character Tracker';
+
+-- ----------------------------
+-- Records of realmcharacters
+-- ----------------------------
+INSERT INTO `realmcharacters` VALUES ('1', '1', '1');
+INSERT INTO `realmcharacters` VALUES ('1', '2', '1');
 
 -- ----------------------------
 -- Table structure for realmlist
@@ -234,8 +291,13 @@ CREATE TABLE `realmlist` (
   `queued` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Realm System';
-INSERT INTO `realmlist` (`id`, `name`, `address`, `port`, `icon`, `flag`, `timezone`, `allowedSecurityLevel`, `population`, `gamebuild`, `online`, `delay`, `queued`) VALUES (1, 'JadeCore', '127.0.0.1', 8085, 0, 0, 1, 0, 0, 18019, 0, 10, 0);
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='Realm System';
+
+-- ----------------------------
+-- Records of realmlist
+-- ----------------------------
+INSERT INTO `realmlist` VALUES ('1', 'JadeCore', '192.168.0.131', '8085', '0', '0', '1', '0', '0', '18019', '0', '10', '0');
+INSERT INTO `realmlist` VALUES ('2', 'uwow', '192.168.0.131', '8085', '0', '1', '0', '0', '0', '17538', '0', '0', '0');
 
 -- ----------------------------
 -- Table structure for transferts
@@ -249,12 +311,18 @@ CREATE TABLE `transferts` (
   `to` int(11) NOT NULL,
   `revision` blob NOT NULL,
   `dump` longtext NOT NULL,
+  `toacc` int(11) NOT NULL,
+  `transferId` int(11) NOT NULL,
   `last_error` blob NOT NULL,
   `nb_attempt` int(11) NOT NULL,
   `state` int(10) DEFAULT NULL,
   `error` int(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of transferts
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for transferts_logs
@@ -266,8 +334,15 @@ CREATE TABLE `transferts_logs` (
   `perso_guid` int(11) DEFAULT NULL,
   `from` int(2) DEFAULT NULL,
   `to` int(2) DEFAULT NULL,
-  `dump` longtext
+  `dump` longtext,
+  `toacc` int(11) DEFAULT NULL,
+  `newguid` int(11) DEFAULT NULL,
+  `transferId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of transferts_logs
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for uptime
@@ -281,3 +356,7 @@ CREATE TABLE `uptime` (
   `revision` varchar(255) NOT NULL DEFAULT 'Trinitycore',
   PRIMARY KEY (`realmid`,`starttime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Uptime system';
+
+-- ----------------------------
+-- Records of uptime
+-- ----------------------------
