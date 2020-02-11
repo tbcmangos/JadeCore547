430be97a7b8242ffab4469cae5c4fa1fe7d498b4
coolzoom <tommingc@gmail.com>
2020-02-11 16:01:01 +0800

    Revert "Update bot_ai.cpp"
    This reverts commit f78138f8fe338b4c4109f27369293cc66cc1729f.

--- src/server/game/AI/NpcBots/bot_ai.cpp
+++ src/server/game/AI/NpcBots/bot_ai.cpp
@@ -7232,9 +7232,7 @@ void bot_minion_ai::InitOwner()
     //"SELECT owner FROM character_npcbot WHERE entry = ?", CONNECTION_SYNCH
     stmt->setUInt32(0, me->GetEntry());
     PreparedQueryResult result = CharacterDatabase.Query(stmt);
-    if (!result)
-        return;
-    //ASSERT(result);
+    ASSERT(result);
 
     Field* field = result->Fetch();
     _ownerGuid = field[0].GetUInt32();
